54ef4d56c638c8ca4a4baa36e81be386899838b2
caylakpenguen <caylakpenguen@gmail.com>
2016-04-29 22:47:10 +0300

    mini fix

--- yedekle-sistem.sh
+++ yedekle-sistem.sh
@@ -8,23 +8,23 @@ DATADIR="/root/sistem"
 DIZIN="$DATADIR/$(date +%F)"
 
 yedekal(){
-mkdir -p $DIZIN
-cd $DIZIN
-cp -r /etc/bind .
-cp -r /etc/nginx .
-cp -r /etc/supervisor .
-cp -a /etc/aliases* .
-cp -ar /etc/postfix .
-cp -ar /etc/opendkim* .
-cp -ar /var/log/apt apt-log
-cp -ar /var/log/mail.* .
-date > $(date +%F-%H-%M).txt
-cd $DATADIR
-tar -zcf $DIZIN.tar.gz $DIZIN
+	mkdir -p $DIZIN
+	cd $DIZIN
+	cp -r /etc/bind .
+	cp -r /etc/nginx .
+	cp -r /etc/supervisor .
+	cp -a /etc/aliases* .
+	cp -ar /etc/postfix .
+	cp -ar /etc/opendkim* .
+	cp -ar /var/log/apt apt-log
+	cp -ar /var/log/mail.* .
+	date > $(date +%F-%H-%M).txt
+	cd $DATADIR
+	tar -zcf $DIZIN.tar.gz $DIZIN
 }
 
 yedekle (){
-yedekal
+	yedekal
 }
 
 # ***
